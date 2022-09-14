b4237bb8fd16b97eaec87c45453c62daa311c388
Santiago Giraldo <porqueamisg@gmail.com>
2022-09-13 21:20:12 -0500

    Middleware to handle errors has been implemented

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,10 @@
-import { validationResult } from "express-validator";
-
-
 export const register = (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-
-        return res.status(400).json({ errors: errors.array() });
-    }
     console.log(req.body);
-    res.json({ ok: true });
+    res.json({ ok: "Register" });
 };
 
 export const login = (req, res) => {
 
-    res.json({ ok: "login" });
+    res.json({ ok: "Login" });
 };
 
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,24 +1,40 @@
 import express from 'express';
 import { login, register } from '../controllers/auth.controller.js';
 import { body } from 'express-validator';
+import { validationResults } from '../middlewares/validationResults.js';
 const router = express.Router();
 
-router.post("/login", login);
+
 
 router.post("/register", [
     body('email', 'Formato de Email incorrecto')
         .trim()
         .isEmail()
         .normalizeEmail(),
-    body('password', 'Formato de password incorrecto')
+    body('password', 'Minimo 6 caracteres de password')
         .trim()
-        .isLength({ min: 6 })
+        .isLength({ min: 6 }),
+    body('password', 'Formato de password incorrecto')
         .custom((value, { req }) => {
             if (value !== req.body.repassword) {
                 throw new Error('No coinciden el password con previamente introducido')
             }
-        })
-],
-    register);
+            return value;
+        }
+        ),
+], validationResults,
+    register
+);
+router.post("/login", [
+    body('email', 'Formato de Email incorrecto')
+        .trim()
+        .isEmail()
+        .normalizeEmail(),
+    body('password', 'Minimo 6 caracteres de password')
+        .trim()
+        .isLength({ min: 6 }),
+], validationResults,
+    login
 
+);
 export default router;
