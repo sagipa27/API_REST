d0b1bd69f05154c9dc11c53f2c898d7a05285abd
Santiago Giraldo <porqueamisg@gmail.com>
2022-10-03 13:36:36 -0500

    Se desplegara en cloud

--- database/connectDB.js
+++ database/connectDB.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 
 try {
-    await mongoose.connect("mongodb+srv://admin:admin@cluster0.vadxztc.mongodb.net/DBApi");
+    await mongoose.connect(process.env.URI_MONGO);
     console.log("Connect DB OK");
 } catch (error) {
     console.log("Error conecction to MongoDB:" + error.message);
