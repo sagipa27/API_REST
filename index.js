cb222809763d49baa8dbfb7e87bcc2611b9f7ef0
Santiago Giraldo <porqueamisg@gmail.com>
2022-10-03 13:45:00 -0500

    cambios

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ import redirectRouter from './routes/redirect.route.js';
 
 const app = express();
 
-const whiteList = [process.env.ORIGIN1]
+const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2]
 
 app.use(cors({
     origin: function (origin, callback) {
-        if (whiteList.includes(origin)) {
+        if (!origin || whiteList.includes(origin)) {
             return callback(null, origin);
         }
         return callback("Error de CORS origin: " + origin + "no autorizado!");
